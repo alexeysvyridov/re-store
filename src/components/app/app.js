53cc02ea2e003be12996452f3706e88ea7dbb2a9
unknown <alexeysvyridov@gmail.com>
2020-10-09 00:37:09 +0300

    init proj

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './app.css';
 import  { withBookstoreService} from '../hoc';
+import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+
+import { HomePage, CardPage } from '../pages'
 
 const App = ({bookstoreService}) => {
-  console.log(bookstoreService.getBooks());
+ const  books = bookstoreService.getBooks()
   return (
-    <div>App!!</div>
+    <Router>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/card">Card</Link>
+        </li>
+      </ul>
+    </nav>
+
+    <Switch>
+      <Route path="/" component={HomePage} exact/>
+      <Route path="/card" component={CardPage} />
+    </Switch>
+  </Router>
   )
 }
 
---
+++ src/components/pages/index.js
@@ -0,0 +1,8 @@
+import CardPage from './card-page';
+import HomePage from './home-page';
+
+
+export {
+  CardPage,
+  HomePage
+}
